2aff0eec3fadf6c2513acf1a5676cd12546fc4c6
gerillima@gmail.com <gerillima@gmail.com>
2022-10-17 19:32:58 +0800

    first commit

---
+++ prompt-example.js
@@ -0,0 +1,20 @@
+let prompt = require('prompt-sync')();
+
+// let firstName = prompt("Please enter your first name: ");
+// console.log("Hi", firstName);
+
+// let length = prompt("Please enter the length of the area in meters:");
+// let width = prompt("Please enter the width of the area in meters:");
+// area_of_square = length * width;
+// perimeter = 2*length + 2*width;
+// console.log("The area is", area_of_square)
+// console.log("The perimeter is", perimeter)
+
+// let x = 3
+// let y = "4"
+// let z = 5
+
+// ans = z+parseInt(y+x)
+// console.log(ans) 
+
+
